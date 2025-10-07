b3243a48db5d10e10698eafba3377ada45f48b9f
Lukas Lipp <llipp@cg.tuwien.ac.at>
2025-10-07 17:03:30 +0200

    update

--- rtm.py
+++ rtm.py
@@ -1,11 +1,11 @@
 import drjit as dr
-from drjit.cuda.ad import Float, TensorXf, Quaternion4f, Array2u, Array3f
+from drjit.auto.ad import TensorXf
 import mitsuba as mi
 from imgui_bundle import imgui, immapp
 # dr.set_flag(dr.JitFlag.Debug, True)
-mi.set_variant('cuda_ad_rgb')
+mi.set_variant('cuda_ad_rgb' if dr.has_backend(dr.JitBackend.CUDA) else 'llvm_ad_rgb')
 
-class Texture:
+class GlTexture:
     def __init__(self, width, height):
         import OpenGL.GL as gl
         self.width, self.height, self.id = width, height, gl.glGenTextures(1)
@@ -16,12 +16,28 @@ class Texture:
             gl.GL_TEXTURE_2D, 0, gl.GL_RGBA32F, width, height, 0,
             gl.GL_RGBA, gl.GL_FLOAT, None
         )
-        self.interop = dr.cuda.GLInterop.from_texture(self.id)
+        try:
+            self.interop = dr.cuda.GLInterop.from_texture(self.id)
+        except Exception as e:
+            print("Warning: Could not create CUDA-OpenGL interop:", e)
+            self.interop = None
+
+    def upload(self, img):
+        if dr.backend_v(img) == dr.JitBackend.CUDA:
+            self.interop.map().upload(img).unmap()
+        elif dr.backend_v(img) == dr.JitBackend.LLVM:
+            import OpenGL.GL as gl
+            gl.glBindTexture(gl.GL_TEXTURE_2D, self.id)
+            gl.glTexSubImage2D(
+                gl.GL_TEXTURE_2D, 0, 0, 0, self.width, self.height,
+                gl.GL_RGBA, gl.GL_FLOAT, img.to_numpy()
+            )
 
 class Camera:
     def __init__(self):
         self.q, self.c, self.d = dr.scalar.Quaternion4f(0, 0, 0, 1), dr.scalar.Array3f(0, 0, 0), 14.0
         self.resolution, self.scale = dr.scalar.Array2u(256), 1.0
+        self.gl_texture: GlTexture | None = None
         self.sensor = mi.load_dict({
             'type': 'perspective',
             'fov': 39.3077,
@@ -72,36 +88,43 @@ class Camera:
         self.params['film.size'] = self.resolution
         self.film = dr.zeros(TensorXf, shape=(self.resolution[1], self.resolution[0], 4))
         self.params.update()
+
+    def handle_gl_texture(self):
+        ' Only call if gl texture is needed and with a valid opengl context '
+        if not self.gl_texture or self.gl_texture.width != self.resolution[0] or self.gl_texture.height != self.resolution[1]:
+            self.gl_texture = GlTexture(self.resolution[0], self.resolution[1])
+
+    def process_imgui_inputs(self, min, max):
+        from imgui_bundle import imgui
+        io = imgui.get_io()
+        if imgui.is_mouse_clicked(0):
+            self.start_pos = io.mouse_pos - min
+        if imgui.is_mouse_released(0) and self.start_pos:
+            self.q = self.rotate(self.start_pos, io.mouse_pos - min)
+            self.update(self.q)
+            self.start_pos = None
+        if imgui.is_mouse_dragging(0) and self.start_pos:
+            self.update(self.rotate(self.start_pos, io.mouse_pos - min))
+        elif imgui.is_mouse_dragging(1):
+            self.c += dr.quat_apply(self.q, dr.scalar.Array3f(-io.mouse_delta.x, io.mouse_delta.y, 0.0)) * (self.d * 0.002)
+            self.update(self.q)
+        if io.mouse_wheel != 0:
+            self.d = self.d - io.mouse_wheel * 0.3
+            if self.d < 0.1:
+                self.d = 0.1
+            self.update(self.q)
         
 class State:
     def __init__(self, scene_file):
         self.scene = mi.load_file(scene_file)
         self.frame = 0
         self.accumulate = True
-        self.texture: Texture | None = None
         self.camera = Camera()
         self.start_pos = None
 
     def process_inputs(self, min, max):
         if imgui.table_get_hovered_column() == 0:
-            io = imgui.get_io()
-            if imgui.is_mouse_clicked(0):
-                self.start_pos = io.mouse_pos - min
-            if imgui.is_mouse_released(0) and self.start_pos:
-                q = self.camera.rotate(self.start_pos, io.mouse_pos - min)
-                self.start_pos = None
-                self.camera.q = q
-                self.camera.update(q)
-            if imgui.is_mouse_dragging(0) and self.start_pos:
-                self.camera.update(self.camera.rotate(self.start_pos, io.mouse_pos - min))
-            elif imgui.is_mouse_dragging(1):
-                self.camera.c += dr.quat_apply(self.camera.q, dr.scalar.Array3f(-io.mouse_delta.x, io.mouse_delta.y, 0.0)) * (self.camera.d * 0.002)
-                self.camera.update(self.camera.q)
-            if io.mouse_wheel != 0:
-                self.camera.d = self.camera.d - io.mouse_wheel * 0.3
-                if self.camera.d < 0.1:
-                    self.camera.d = 0.1
-                self.camera.update(self.camera.q)
+            self.camera.process_imgui_inputs(min, max)
 
 state = State("scenes/simple.xml")
 
@@ -110,21 +133,20 @@ def render(scene, texture, camera, seed=0, sensor=0):
     return mi.render(scene, spp=1, seed=seed, sensor=sensor)
 
 def show_image(width, height):
-    if state.texture is None or state.texture.width != width or state.texture.height != height:
-        state.texture = Texture(width, height)
-    img = render(state.scene, state.texture, state.camera, seed=mi.UInt(state.frame), sensor=state.camera.sensor)
+    state.camera.handle_gl_texture()
+    img = render(state.scene, state.camera.gl_texture, state.camera, seed=mi.UInt(state.frame), sensor=state.camera.sensor)
     if img.shape[2] == 3:
         # Add alpha channel if not present
         alpha = dr.ones(TensorXf, shape=(img.shape[0], img.shape[1], 1))
         img = dr.concat((TensorXf(img), alpha), axis=2)
     state.camera.film = state.camera.film + img if state.accumulate else img
     # use alpha channel to store the sample count
-    state.texture.interop.map().upload(dr.linear_to_srgb(state.camera.film / state.camera.film[0][0][3]) ).unmap()
+    state.camera.gl_texture.upload(dr.linear_to_srgb(state.camera.film / state.camera.film[0][0][3]) )
     state.frame += 1
     w, h = width * state.camera.scale, height * state.camera.scale
     imgui.set_cursor_pos_x(imgui.get_column_width()//2 - w//2)
     imgui.set_cursor_pos_y(imgui.get_window_viewport().size.y//2 - h//2)
-    imgui.image(imgui.ImTextureRef(state.texture.id), (w, h))
+    imgui.image(imgui.ImTextureRef(state.camera.gl_texture.id), (w, h))
     state.process_inputs((imgui.get_column_width()//2 - w//2, imgui.get_window_viewport().size.y//2 - h//2), (imgui.get_column_width()//2 + w//2, imgui.get_window_viewport().size.y//2 + h//2))
 
 def gui():
